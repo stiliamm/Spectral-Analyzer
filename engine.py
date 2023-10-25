a95bcbc10a87b95f541acb053f12cd122d97149f
stiliamm <redemption.records@icloud.com>
2023-10-25 11:48:52 +0300

    update

--- engine.py
+++ engine.py
@@ -7,32 +7,30 @@ from stream import Stream
 
 
 class Engine:
-    frames = []
-    driver = AudioDriver()
-    stream = Stream()
-    plotter = Plotter()
+    def __init__(self) -> None:
+        self.frames = []
+        self.driver = AudioDriver()
+        self.stream = Stream()
+        self.plotter = Plotter()
 
-    # Main Loop
-    while True:
-        # read data from input and calculate the FFT
-        data = stream.stream.read(stream.chunk, exception_on_overflow=False)
-        data_int = struct.unpack(str(stream.chunk) + 'h', data)
-        fft_data = np.abs(np.fft.fft(data_int)) * 2 / (11000 * stream.chunk)
-        if len(fft_data) == 0:
-            continue
+    def run(self):
+        while True:
+            data = self.stream.stream.read(self.stream.chunk, exception_on_overflow=False)
+            data_int = struct.unpack(str(self.stream.chunk) + 'h', data)
+            fft_data = np.abs(np.fft.fft(data_int)) * 2 / (11000 * self.stream.chunk)
+            if len(fft_data) == 0:
+                continue
 
-        # update the plot
-        plotter.update_plot(data_int, fft_data)
+            self.plotter.update_plot(data_int, fft_data)
 
-        # command line for recording
-        if keyboard.is_pressed("r"):
-            print("Recording... Press SPACEBAR to stop.")
-            frames.append(data)
+            if keyboard.is_pressed("r"):
+                print("Recording... Press SPACEBAR to stop.")
+                self.frames.append(data)
 
-        if keyboard.is_pressed(" "):
-            plotter.plot_record(frames)
-            print("Recording finished.")
-
+            if keyboard.is_pressed(" "):
+                self.plotter.plot_record(self.frames)
+                print("Recording finished.")
 
 if __name__ == "__main__":
-    Engine()
+    e = Engine()
+    e.run()
--- plotter.py
+++ plotter.py
@@ -16,13 +16,10 @@ class Plotter:
         self.x = np.arange(0, 2 * self.chunk, 2)
         self.xf = np.linspace(0, self.rate, self.chunk)
 
-        # create a linear object with random data
         self.line_waveform, = self.ax_waveform.plot(self.x, np.random.rand(stream.chunk), 'w')
 
-        # create logarithmic line for spectrum
         self.line_spectrum, = self.ax_spectrum.loglog(self.xf, np.random.rand(stream.chunk), 'y')
 
-        # format waveform axes
         self.ax_waveform.set_title('Audio Waveform')
         self.ax_waveform.set_xlabel('Samples')
         self.ax_waveform.set_ylabel('Volume')
@@ -30,7 +27,6 @@ class Plotter:
         self.ax_waveform.set_xlim(0, stream.chunk)
         self.ax_waveform.grid('y', color='w', linestyle='-', linewidth='0.2')
 
-        # format spectrum axes
         self.ax_spectrum.set_title('Audio Spectrum')
         self.ax_spectrum.set_ylabel("dBFs")
         self.ax_spectrum.set_xlabel("Hz")
@@ -39,26 +35,20 @@ class Plotter:
         self.ax_spectrum.set_xlim(20, stream.rate / 2)
         self.ax_spectrum.grid('y', color='w', linestyle='-', linewidth='0.2')
 
-        # show axes
         self.fig.tight_layout()
         self.fig.show()
 
     def update_plot(self, waveform_data, spectrum_data):
-        # update waveform and spectrum data
         self.line_waveform.set_ydata(waveform_data)
         self.line_spectrum.set_ydata(spectrum_data)
-
-        # update the plot
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
     def plot_record(self, frames):
-        # calculate the fft data
         audio_data = np.frombuffer(b''.join(frames), dtype=np.int16)
         lin_y = np.abs(np.fft.fft(audio_data) * 2 / (11000 * len(frames)))
         lin_x = np.linspace(0, len(frames), len(lin_y))
 
-        # plot the recorded data
         plt.figure(figsize=(10, 7))
         plt.loglog(lin_x, lin_y)
         plt.title('Audio Spectrum')
