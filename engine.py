e32baad3b39faf5414595a78b0b6d243c0522463
stiliamm <129324277+stiliamm@users.noreply.github.com>
2024-07-17 13:50:24 +0300

    Update engine.py

--- engine.py
+++ engine.py
@@ -9,9 +9,9 @@ from stream import Stream
 class Engine:
     
     def __init__(self) -> None:
-        self.frames = []
-        self.driver = AudioDriver()
-        self.stream = Stream()
+        self.frames  = []
+        self.driver  = AudioDriver()
+        self.stream  = Stream()
         self.plotter = Plotter()
 
     def run(self):
@@ -19,8 +19,8 @@ class Engine:
             data = self.stream.stream.read(self.stream.chunk, exception_on_overflow=False)
             data_int = struct.unpack(str(self.stream.chunk) + 'h', data)
             fft_data = np.abs(np.fft.fft(data_int)) * 2 / (11000 * self.stream.chunk)
-            if len(fft_data) == 0:
-                continue
+            
+            continue if len(fft_data) == 0
 
             self.plotter.update_plot(data_int, fft_data)
 
